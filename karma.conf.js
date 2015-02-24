72a4fcacf0e00dc577b578e8022cb38a87eb18b2
Cedric Gatay <cedric@gatay.fr>
2015-02-24 12:05:38 +0100

    Added Karma-Junit-Reporter
    Updated karma to produce junit compatible test-results.xml file (to be used by CI server)

--- karma.conf.js
+++ karma.conf.js
@@ -51,6 +51,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: false,
+    reporters: ['junit']
   });
 };
